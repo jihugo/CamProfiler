1c2c5aa1bc9a41e361f0209045d2ee15117fbd6a
jihugo <yugeji2@illinois.edu>
2023-01-22 14:17:28 -0600

    add circular linear interpolation function

--- camprofiler/utilities.py
+++ camprofiler/utilities.py
@@ -1,5 +1,5 @@
 """Utilities module including useful tools"""
-__all__ = ["circular_convolve"]
+__all__ = ["circular_convolve", "circular_linear_interp"]
 
 import warnings
 import numpy as np
@@ -31,3 +31,20 @@ def circular_convolve(arr: np.ndarray, kernel: np.ndarray) -> np.ndarray:
             tip[: int(k / 2)],
         ]
     )
+
+
+def circular_linear_interp(arr: np.ndarray, new_size: int) -> np.ndarray:
+    """Perform linear interpolation
+
+    The first data point in arr is
+
+    Parameters
+    ----------
+    arr : 1D NumPy ndarray
+    new_size : int
+    """
+
+    arr_modified = np.append(arr, arr[0])
+    x_arr_curr = np.arange(arr.shape[0] + 1)
+    x_arr_new = np.linspace(start=0, stop=arr.shape[0], num=new_size + 1)
+    return np.interp(x_arr_new, x_arr_curr, arr_modified)[:-1]
